6fd2fc51d071ee3b6311cc634f73dc8e623e2803
Paweł Kęska <projekty@pawelkeska.eu>
2024-02-06 23:03:13 +0100

    [Packages] Add utils

---
+++ packages/common/utils/README.md
@@ -0,0 +1,11 @@
+# utils
+
+Utils package.
+
+## Building
+
+Run `nx build utils` to build the library.
+
+## Running unit tests
+
+Run `nx test utils` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/common/utils/src/check-types.ts
@@ -0,0 +1,73 @@
+/**
+ * Check is function .
+ * @param item
+ * @returns {boolean}
+ */
+export function isFunction(item: any): boolean {
+  return item && typeof item === 'function' && !Array.isArray(item);
+}
+
+/**
+ * Simple object check.
+ * @param item
+ * @returns {boolean}
+ * From https://stackoverflow.com/a/34749873/772859
+ */
+export function isObject(item: any) {
+  return item && typeof item === 'object' && !Array.isArray(item);
+}
+
+/**
+ * Check is object or function.
+ * @param item
+ * @returns {boolean}
+ */
+export function isObjectOrFunction(item: any): boolean {
+  return isFunction(item) || isObject(item);
+}
+
+/**
+ * Check is class instance.
+ * @param item
+ * @returns {boolean}
+ */
+export function isClassInstance(item: any): boolean {
+  return isObject(item) && item.constructor.name !== 'Object';
+}
+
+/**
+ * Check value not empty.
+ * @param item
+ * @returns {boolean}
+ */
+export function isNotEmpty(item: any): boolean {
+  return !isEmpty(item);
+}
+
+/**
+ * Check value empty.
+ * @param item
+ * @returns {boolean}
+ */
+export function isEmpty(item: any) {
+  if (item instanceof Array) {
+    item = item.filter((val) => !isEmpty(val));
+    return item.length === 0;
+  } else if (item && typeof item === 'object') {
+    return Object.keys(item).length === 0;
+  } else {
+    return (
+      !item ||
+      (item + '').toLocaleLowerCase() === 'null' ||
+      (item + '').toLocaleLowerCase() === 'undefined'
+    );
+  }
+}
+
+export function isJsObject(object: any) {
+  return object !== null && object !== undefined && typeof object === 'object';
+}
+
+export function isNumeric(val: any): boolean {
+  return !(val instanceof Array) && (val - parseFloat(val) + 1) >= 0;
+}
