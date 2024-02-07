aef42754c531ae416533ccfbdd880e396fe0d0e0
Paweł Kęska <projekty@pawelkeska.eu>
2024-02-07 14:49:48 +0100

    [Api] Building blocks

---
+++ libs/api/building-blocks/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: '@owl-app/lib-api-bulding-blocks',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/libs/api/building-blocks',
+};
