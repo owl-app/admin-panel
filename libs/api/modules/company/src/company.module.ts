e8288c68caae88dee6b1f8b6570e8ed1c3cedb3a
Paweł Kęska <projekty@pawelkeska.eu>
2024-07-02 23:05:34 +0200

    [Api][Company] Add module

---
+++ libs/api/modules/company/src/company.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { JwtStrategy } from '@owl-app/lib-api-bulding-blocks/passport/jwt.strategy'
+
+import config, { JwtConfigProvider  } from '@owl-app/lib-api-bulding-blocks/config'
+import { DatabaseModule } from '@owl-app/lib-api-bulding-blocks/database/database.module'
+
+import { AuthModule } from './auth/auth.module'
+import { UserModule } from './user/user.module'
+
+@Module({
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // this not work in nx
+      envFilePath: [`.env.${process.env.NODE_ENV}`, `.env`],
+      load: config,
+    }),
+    AuthModule,
+    UserModule
+  ],
+  providers: [
+    JwtConfigProvider,
+    JwtStrategy
+  ]
+})
+export class UserAccessModule {}
