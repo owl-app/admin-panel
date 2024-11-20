cd14c1b84ca35d3db0533d67da8b7d5770068533
Paweł Kęska <projekty@pawelkeska.eu>
2024-11-20 17:19:53 +0100

    Clear bootstrap

--- apps/api/src/bootstrap.ts
+++ apps/api/src/bootstrap.ts
@@ -1,11 +1,7 @@
 import { ClassSerializerInterceptor, INestApplication } from '@nestjs/common'
 import { NestFactory, Reflector } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
-// // import { SentryService } from '@ntegral/nestjs-sentry';
-// import { useContainer } from 'class-validator';
-// import * as helmet from 'helmet';
 import chalk from 'chalk'
-// import { join } from 'path';
 import { urlencoded, json } from 'express'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { ConfigService } from '@nestjs/config'
@@ -13,17 +9,6 @@ import { APP_CONFIG_NAME, IConfigApp } from '@owl-app/lib-api-core/config'
 import { JwtAuthGuard } from '@owl-app/lib-api-core/passport/jwt.guard'
 import { RoutePermissionGuard } from '@owl-app/lib-api-core/rbac/guards/route-permission.guard'
 import cookieParser from 'cookie-parser'
-// import { HttpExceptionsFilter } from '../http';
-// import { EntitySubscriberInterface } from 'typeorm';
-// // import { IPluginConfig } from '@gauzy/common';
-// import { getConfig, setConfig, environment as env } from '@gauzy/config';
-// import { getEntitiesFromPlugins } from '@gauzy/plugin';
-// import { coreEntities } from '../core/entities';
-// import { coreSubscribers } from './../core/entities/subscribers';
-// import { AppService } from '../app.service';
-// import { AppModule } from '../app.module';
-// import { AuthGuard } from './../shared/guards';
-// import { SharedModule } from './../shared/shared.module';
 
 export async function bootstrap(
     bootstrapModule: any
@@ -35,7 +20,6 @@ export async function bootstrap(
     const allowedHeaders = [
         'Authorization',
         'Language',
-        'Tenant-Id',
         'X-Requested-With',
         'X-Auth-Token',
         'X-HTTP-Method-Override',
@@ -105,96 +89,9 @@ export async function bootstrap(
 
     console.log(chalk.magenta(`Swagger UI at http://${host}:${port}/swg`));
 
-    // /**
-    //  * Dependency injection with class-validator
-    //  */
-    // // useContainer(app.select(SharedModule), { fallbackOnErrors: true });
-
     await app.listen(port, host, () => {
     	console.log(chalk.magenta(`Listening at http://${host}:${port}/${globalPrefix}`));
     });
 
     return app;
 }
-
-// /**
-//  * Setting the global config must be done prior to loading the Bootstrap Module.
-//  */
-// export async function registerPluginConfig(
-// 	pluginConfig: Partial<IPluginConfig>
-// ) {
-// 	if (Object.keys(pluginConfig).length > 0) {
-// 		setConfig(pluginConfig);
-// 	}
-
-// 	/**
-// 	 * Configure migration settings
-// 	 */
-// 	setConfig({
-// 		dbConnectionOptions: {
-// 			...getMigrationsSetting()
-// 		}
-// 	});
-
-// 	console.log(
-// 		chalk.green(
-// 			`DB Config: ${JSON.stringify(getConfig().dbConnectionOptions)}`
-// 		)
-// 	);
-
-// 	/**
-// 	 * Registered core & plugins entities
-// 	 */
-// 	const entities = await registerAllEntities(pluginConfig);
-// 	setConfig({
-// 		dbConnectionOptions: {
-// 			entities,
-// 			subscribers: coreSubscribers as Array<Type<EntitySubscriberInterface>>,
-// 		}
-// 	});
-
-// 	let registeredConfig = getConfig();
-// 	return registeredConfig;
-// }
-
-// /**
-//  * Returns an array of core entities and any additional entities defined in plugins.
-//  */
-// export async function registerAllEntities(
-// 	pluginConfig: Partial<IPluginConfig>
-// ) {
-// 	const allEntities = coreEntities as Array<Type<any>>;
-// 	const pluginEntities = getEntitiesFromPlugins(pluginConfig.plugins);
-
-// 	for (const pluginEntity of pluginEntities) {
-// 		if (allEntities.find((e) => e.name === pluginEntity.name)) {
-// 			throw new ConflictException({
-// 				message: `error.${pluginEntity.name} conflict by default entities`
-// 			});
-// 		} else {
-// 			allEntities.push(pluginEntity);
-// 		}
-// 	}
-// 	return allEntities;
-// }
-
-// /**
-//  * GET migrations directory & CLI paths
-//  *
-//  * @returns
-//  */
-// export function getMigrationsSetting() {
-
-// 	console.log(`Reporting __dirname: ${__dirname}`);
-
-// 	//TODO: We need to define some dynamic path here
-// 	return {
-// 		migrations: [
-// 			// join(__dirname, '../../src/database/migrations/*{.ts,.js}'),
-// 			join(__dirname, '../database/migrations/*{.ts,.js}')
-// 		],
-// 		cli: {
-// 			migrationsDir: join(__dirname, '../../src/database/migrations')
-// 		},
-// 	}
-// }
